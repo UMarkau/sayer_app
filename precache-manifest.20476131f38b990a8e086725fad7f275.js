3a7526ef833cd110bc65f27e51839f14981b6f97
Uladzimir Markau <kitanokiller@gmail.com>
2019-01-29 23:15:52 +0300

    Updates

---
+++ precache-manifest.20476131f38b990a8e086725fad7f275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3954437408658ee2949",
+    "url": "/static/js/main.d3954437.chunk.js"
+  },
+  {
+    "revision": "75e3cd0ec349e101c9f3",
+    "url": "/static/js/1.75e3cd0e.chunk.js"
+  },
+  {
+    "revision": "d3954437408658ee2949",
+    "url": "/static/css/main.c65f0aeb.chunk.css"
+  },
+  {
+    "revision": "d3f7309a8778f1661d93757f7d0adc17",
+    "url": "/index.html"
+  }
+];
